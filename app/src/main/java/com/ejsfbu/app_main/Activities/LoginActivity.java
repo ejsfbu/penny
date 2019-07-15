715f07a9a77dba95ad0282b5937323769727204a
Jordan Peel <jordanep@fb.com>
2019-07-15 14:18:08 -0700

    login protocalls

--- app/src/main/java/com/ejsfbu/app_main/Activities/LoginActivity.java
+++ app/src/main/java/com/ejsfbu/app_main/Activities/LoginActivity.java
@@ -3,17 +3,57 @@ package com.ejsfbu.app_main.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ejsfbu.app_main.R;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 // app will automatically load to this screen, unless user already logged in
 // upon login app will navigate to goals list fragment in main activity
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button bLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        bLogin = findViewById(R.id.bLogin);
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
+
+                login(username, password);
+            }
+        });
+    }
+
+    private void login(String username, String password) {
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login Failure", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
