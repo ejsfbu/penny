9dfa6fc1c9eaafa62b86376ee53515538023a920
Jordan Peel <jordanep@fb.com>
2019-07-15 12:05:36 -0700

    now what

---
+++ app/src/main/java/com/ejsfbu/app_main/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ejsfbu.app_main;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
