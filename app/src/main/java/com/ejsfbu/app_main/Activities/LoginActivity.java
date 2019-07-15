d11f924e20b8334e52d8b2f82124dc048c0d8d45
Jordan Peel <jordanep@fb.com>
2019-07-15 14:31:12 -0700

    sign up naviagation

--- app/src/main/java/com/ejsfbu/app_main/Activities/LoginActivity.java
+++ app/src/main/java/com/ejsfbu/app_main/Activities/LoginActivity.java
@@ -2,8 +2,8 @@ package com.ejsfbu.app_main.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.etUsername) EditText etUsername;
     @BindView(R.id.etPassword) EditText etPassword;
     @BindView(R.id.bLogin) Button bLogin;
+    @BindView(R.id.bSignUp) Button bSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +40,13 @@ public class LoginActivity extends AppCompatActivity {
         final String password = etPassword.getText().toString();
 
         login(username, password);
+    }
+
+    @OnClick(R.id.bSignUp)
+    public void clickSignUp() {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     private void login(String username, String password) {
