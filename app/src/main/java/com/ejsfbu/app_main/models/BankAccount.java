f7900552f216fe91308c134654da79f45ae14791
Ethan Alexander Horoschak <ehoroschak@fb.com>
2019-07-15 14:16:07 -0700

    config parseapp

--- app/src/main/java/com/ejsfbu/app_main/models/BankAccount.java
+++ app/src/main/java/com/ejsfbu/app_main/models/BankAccount.java
@@ -3,7 +3,6 @@ package com.ejsfbu.app_main.models;
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
-@ParseClassName("User")
-public class User extends ParseObject {
-
+@ParseClassName("BankAccount")
+public class BankAccount extends ParseObject {
 }
