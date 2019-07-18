cf7ca14654b56963758e04be03f862b1affeb65d
Jordan Peel <jordanep@fb.com>
2019-07-18 13:50:02 -0700

    proper logic in place but whereEqualTo not working as expected

--- app/src/main/java/com/ejsfbu/app_main/Activities/SignUpActivity.java
+++ app/src/main/java/com/ejsfbu/app_main/Activities/SignUpActivity.java
@@ -1,29 +1,14 @@
 package com.ejsfbu.app_main.Activities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
 import com.ejsfbu.app_main.Fragments.SignupGetStarted;
-import com.ejsfbu.app_main.Fragments.SignupNameFragment;
 import com.ejsfbu.app_main.R;
 import com.ejsfbu.app_main.models.User;
-import com.parse.ParseException;
-import com.parse.SignUpCallback;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 // new user can navigate to this screen from login activity
 // upon signup app will navigate to create a goal activity/fragment
@@ -45,7 +30,6 @@ public class SignUpActivity extends AppCompatActivity {
         Fragment getStarted = new SignupGetStarted();
         fragmentManager.beginTransaction().replace(R.id.flSignUpContainer, getStarted).commit();
     }
-
 
 
 }
