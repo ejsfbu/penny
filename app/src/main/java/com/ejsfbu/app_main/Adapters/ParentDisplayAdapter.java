63b666f2deac167c3a6c958be58abc0cefc5b24a
Sarah Adebabay <sarahadebabay@fb.com>
2019-07-18 17:49:25 -0700

    created parent display adapter

--- app/src/main/java/com/ejsfbu/app_main/Adapters/ParentDisplayAdapter.java
+++ app/src/main/java/com/ejsfbu/app_main/Adapters/ParentDisplayAdapter.java
@@ -1,4 +1,63 @@
 package com.ejsfbu.app_main.Adapters;
 
-public class ParentDisplayAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
+import com.ejsfbu.app_main.R;
+import com.ejsfbu.app_main.models.User;
+import com.parse.ParseFile;
+
+import java.util.ArrayList;
+
+public class ParentDisplayAdapter extends ArrayAdapter<User> {
+
+    public ParentDisplayAdapter(Context context, ArrayList<User> users) {
+        super(context, 0, users);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the parent item for this position
+        User user = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_parent, parent, false);
+        }
+        // Lookup view for data population
+        TextView tvName = convertView.findViewById(R.id.tv_parent_name);
+        ImageView ivPic = convertView.findViewById(R.id.ivParentImage);
+        // Populate the data into the template view using the data object
+        tvName.setText(user.getName());
+
+
+        ParseFile image = user.getParseFile("profileImage");
+        if (image != null) {
+            String imageUrl = image.getUrl();
+            imageUrl = imageUrl.substring(4);
+            imageUrl = "https" + imageUrl;
+            RequestOptions options = new RequestOptions();
+            options.placeholder(R.drawable.ic_iconfinder_icons_user_1564534)
+                    .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+                    .error(R.drawable.ic_iconfinder_icons_user_1564534)
+                    .transform(new CenterCrop())
+                    .transform(new CircleCrop());
+            Glide.with(getContext())
+                    .load(imageUrl)
+                    .apply(options) // Extra: round image corners
+                    .into(ivPic);
+        }
+        // Return the completed view to render on screen
+        return convertView;
+    }
 }
