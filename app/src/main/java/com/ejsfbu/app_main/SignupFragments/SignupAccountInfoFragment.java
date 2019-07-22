e454ed249c6551b9eb70efd1824d7375cde31e10
Jordan Peel <jordanep@fb.com>
2019-07-22 15:33:51 -0700

    signup bumps to add goal

--- app/src/main/java/com/ejsfbu/app_main/SignupFragments/SignupAccountInfoFragment.java
+++ app/src/main/java/com/ejsfbu/app_main/SignupFragments/SignupAccountInfoFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.ejsfbu.app_main.Activities.AddGoalActivity;
 import com.ejsfbu.app_main.Activities.MainActivity;
 import com.ejsfbu.app_main.R;
 import com.ejsfbu.app_main.models.User;
@@ -106,7 +107,7 @@ public class SignupAccountInfoFragment extends Fragment {
                         Log.d(TAG, "Sign Up Success");
 
                         Intent intent = new Intent(getActivity(),
-                                MainActivity.class);
+                                AddGoalActivity.class);
                         startActivity(intent);
                         getActivity().finish();
                     } else {
