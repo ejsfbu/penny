5433dee06f4d0d59a6655e94da13d63166065f56
Sarah Adebabay <sarahadebabay@fb.com>
2019-07-23 14:17:04 -0700

    you can now delete a goal from your list

--- app/src/main/java/com/ejsfbu/app_main/EditFragments/CancelGoalDialogFragment.java
+++ app/src/main/java/com/ejsfbu/app_main/EditFragments/CancelGoalDialogFragment.java
@@ -10,20 +10,35 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.ejsfbu.app_main.R;
+import com.ejsfbu.app_main.models.Goal;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.Unbinder;
 
 public class CancelGoalDialogFragment extends DialogFragment {
 
     Context context;
+    Button exit_btn;
+    Button comp_cancel_btn;
+    Button transfer_opt_btn;
+    Unbinder unbinder;
+    static Goal currentGoal;
 
     public CancelGoalDialogFragment() {
         // Empty constructor is required for DialogFragment
@@ -31,11 +46,12 @@ public class CancelGoalDialogFragment extends DialogFragment {
         // Use `newInstance` instead as shown below
     }
 
-    public static CancelGoalDialogFragment newInstance(String title) {
+    public static CancelGoalDialogFragment newInstance(String title, Goal goal) {
         CancelGoalDialogFragment frag = new CancelGoalDialogFragment();
         Bundle args = new Bundle();
         args.putString("title", title);
         frag.setArguments(args);
+        currentGoal = goal;
         return frag;
     }
 
@@ -49,6 +65,11 @@ public class CancelGoalDialogFragment extends DialogFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        exit_btn = view.findViewById(R.id.exit_btn);
+        comp_cancel_btn = view.findViewById(R.id.comp_cancel_btn);
+        transfer_opt_btn = view.findViewById(R.id.transfer_opt_btn);
+
+        setClickers();
     }
 
     public void onResume() {
@@ -66,25 +87,38 @@ public class CancelGoalDialogFragment extends DialogFragment {
     }
 
 
-    @OnClick(R.id.comp_cancel_btn)
-    public void compCancel(){
-//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Favourite");
-//        query.whereEqualTo("Tag", "#" + keyword);
-//        query.whereEqualTo("User", ParseUser.getCurrentUser());
-//        query.getFirstInBackground(new FindCallBack() {
-//
-//            @Override
-//            public void done(ParseObject object, com.parse.ParseException arg0) {
-//                // TODO Auto-generated method stub
-//                object.delete();
-//                object.saveInBackground();
-//            }
-//        }););
+    public void setClickers(){
+
+        //cancel option
+        comp_cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //deletes the goal
+                Goal.Query query = new Goal.Query();
+                query.whereEqualTo("objectId", currentGoal.getObjectId());
+                query.findInBackground(new FindCallback<Goal>() {
+                    @Override
+                    public void done(List<Goal> objects, ParseException e) {
+                        if (e == null) {
+                            objects.get(0).deleteInBackground();
+                            objects.get(0).saveInBackground();
+                        } else {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+
+
     }
 
     @OnClick(R.id.transfer_opt_btn)
     public void onClickTransfer() {
         //find the goal in Parse and delete it before that you want to add that amount saved to the goal that you selected from the screen
+
+
         //i think it would be great to implement a search bar and you can select the goal you want out of all the options you have
         //later can grey out that option if there is only 1 goal in your list
     }
