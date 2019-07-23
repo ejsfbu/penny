8d0b79b9d24e62214746234033e01f06555fb0d3
Jordan Peel <jordanep@fb.com>
2019-07-23 15:28:32 -0700

    adding child fields

--- app/src/main/java/com/ejsfbu/app_main/Activities/ChildDetailActivity.java
+++ app/src/main/java/com/ejsfbu/app_main/Activities/ChildDetailActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ejsfbu.app_main.R;
+import com.ejsfbu.app_main.models.User;
 
 public class ChildDetailActivity extends AppCompatActivity {
+
+    private User child;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_detail);
+
+        String childCode = getIntent().getStringExtra("childCode");
     }
 }
