e9d2881ee3825156005464d2c935f481e5a0d73a
Jordan Peel <jordanep@fb.com>
2019-07-23 15:55:30 -0700

    child name, account code, and profile image load

--- app/src/main/java/com/ejsfbu/app_main/Activities/ChildDetailActivity.java
+++ app/src/main/java/com/ejsfbu/app_main/Activities/ChildDetailActivity.java
@@ -3,19 +3,83 @@ package com.ejsfbu.app_main.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.RequestOptions;
 import com.ejsfbu.app_main.R;
 import com.ejsfbu.app_main.models.User;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ChildDetailActivity extends AppCompatActivity {
 
     private User child;
+
+    @BindView(R.id.tvChildDetailName)
+    TextView tvChildDetailName;
+    @BindView(R.id.tvChildDetailAccountCode)
+    TextView tvChildDetailAccountCode;
+
+    @BindView(R.id.ivChildDetailProfilePic)
+    ImageView ivChildDetailProfilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_detail);
 
+        ButterKnife.bind(this);
+
         String childCode = getIntent().getStringExtra("childCode");
+        getChildFromCode(childCode);
+    }
+
+    public void getChildFromCode(String childCode) {
+        User.Query userQuery = new User.Query();
+        userQuery.whereEqualTo("objectId", childCode);
+        userQuery.findInBackground(new FindCallback<User>() {
+            @Override
+            public void done(List<User> objects, ParseException e) {
+                if (e == null) {
+                    child = objects.get(0);
+                    fillData();
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void fillData() {
+        tvChildDetailName.setText(child.getName());
+
+        String accountCode = "Account Code: " + child.getObjectId();
+        tvChildDetailAccountCode.setText(accountCode);
+
+        ParseFile image = child.getProfilePic();
+        if (image != null) {
+            String imageUrl = image.getUrl();
+            imageUrl = imageUrl.substring(4);
+            imageUrl = "https" + imageUrl;
+            RequestOptions options = new RequestOptions();
+            options.placeholder(R.drawable.ic_iconfinder_icons_user_1564534)
+                    .error(R.drawable.ic_iconfinder_icons_user_1564534)
+                    .transform(new CenterCrop())
+                    .transform(new CircleCrop());
+            Glide.with(ChildDetailActivity.this)
+                    .load(imageUrl)
+                    .apply(options) // Extra: round image corners
+                    .into(ivChildDetailProfilePic);
+        }
     }
 }
