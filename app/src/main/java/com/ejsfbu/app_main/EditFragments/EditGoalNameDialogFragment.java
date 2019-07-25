7f1df0ce54f3e96b30400599577ed68af6a8b6d2
Sarah Adebabay <sarahadebabay@fb.com>
2019-07-25 10:45:34 -0700

    finished layout for the dialog fragments that pop up from goal details page

---
+++ app/src/main/java/com/ejsfbu/app_main/EditFragments/EditGoalNameDialogFragment.java
@@ -0,0 +1,69 @@
+package com.ejsfbu.app_main.EditFragments;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import com.ejsfbu.app_main.R;
+import com.ejsfbu.app_main.models.Goal;
+
+import java.util.ArrayList;
+
+public class EditGoalNameDialogFragment extends DialogFragment {
+
+    Context context;
+
+    public EditGoalNameDialogFragment() {
+
+    }
+
+    public static EditGoalNameDialogFragment newInstance(String title, Goal goal) {
+        EditGoalNameDialogFragment frag = new EditGoalNameDialogFragment();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        context = getContext();
+        return inflater.inflate(R.layout.fragment_edit_goal_name, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    public void sendBackResult() {
+        dismiss();
+    }
+
+    public void onResume() {
+        Window window = getDialog().getWindow();
+        Point size = new Point();
+        Display display = window.getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+        window.setLayout((int) (size.x * 0.85), WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setGravity(Gravity.CENTER);
+        super.onResume();
+    }
+
+    private void setOnClick() {
+
+    }
+}
