0097a7e818e6ca46ba8266e4c41abc67b0139dd6
Jordan Peel <jordanep@fb.com>
2019-07-29 15:31:46 -0700

    added colors & badge files

--- app/src/main/java/com/ejsfbu/app_main/DialogFragments/NeedsParentDialogFragment.java
+++ app/src/main/java/com/ejsfbu/app_main/DialogFragments/NeedsParentDialogFragment.java
@@ -99,15 +99,12 @@ public class NeedsParentDialogFragment extends DialogFragment {
     }
 
     public void onResume() {
-        // Store access variables for window and blank point
         Window window = getDialog().getWindow();
         Point size = new Point();
-        // Store dimensions of the screen in `size`
         Display display = window.getWindowManager().getDefaultDisplay();
         display.getSize(size);
         window.setLayout((int) (size.x * 0.9), WindowManager.LayoutParams.WRAP_CONTENT);
         window.setGravity(Gravity.CENTER);
-        // Call super onResume after sizing
         super.onResume();
     }
 
