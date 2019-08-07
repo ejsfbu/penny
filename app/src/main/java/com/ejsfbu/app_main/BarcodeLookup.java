47bd086bf14a829147117106bfd48c63f673557a
Ethan Alexander Horoschak <ehoroschak@fb.com>
2019-08-06 17:43:07 -0700

    set up async and product model

---
+++ app/src/main/java/com/ejsfbu/app_main/BarcodeLookup.java
@@ -0,0 +1,32 @@
+package com.ejsfbu.app_main;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.ejsfbu.app_main.Models.Product;
+import com.ejsfbu.app_main.Models.Request;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
+
+import java.util.Collections;
+
+public class BarcodeLookup {
+    // Constant values for API requests
+    public static final String BASE_URL = "https://api.barcodelookup.com/v2/products";
+    public static final String API_KEY_PARAM = "key";
+    public static final String API_KEY_BARCODE = "barcode";
+    public static final String API_KEY_FORMAT = "formatted";
+    // Tag for logging activity from api requests
+    public static final String TAG = "BarcodeAPI";
+
+    private AsyncHttpClient client1;
+
+    public static Product lookUpItem(Context context, String barcode) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.add(API_KEY_BARCODE, barcode);
+        params.add(API_KEY_FORMAT, "y");
+        params.add(API_KEY_PARAM, context.getResources().getString(R.string.barcode_api_key));
+        // TODO
+    }
+}
